66ef362908a5f64c44e800fc46cec62b85beada7
Carlos Neto <carlosn7to@gmail.com>
2022-08-26 17:00:15 -0300

    adicionado cancelamento à comissão

---
+++ src/components/ageRv/dashboards/ProjectionSection.vue
@@ -0,0 +1,112 @@
+<template>
+  <div class="projection"
+       :class="{'mode-l' : mode === 'light' || mode === undefined,
+                'mode-d' : mode === 'dark',
+                'decrease-menu' : menu === 'decrease'}">
+    <h1>Projeção de vendas para o mês atual</h1>
+    <div class="data-projection">
+      <p>
+        Hoje é dia <b>26</b>, faltam <b>5</b> dias para acabar seu mês e caso mantenha esse desempenho, terminará o mês com:
+      </p>
+      <div class="table-projection">
+        <div class="item-table">
+          <i class="fi fi-ss-stars"></i>
+          <span>Estrelas:</span>
+          <span>2812</span>
+        </div>
+        <div class="item-table" style="color: #009688;">
+          <i class="fi fi-ss-rocket-lunch"></i>
+          <span>Vendas:</span>
+          <span>22</span>
+        </div>
+        <div class="item-table" style="color: #C571E1">
+          <i class="fi fi-sr-chart-line-up"></i>
+          <span>Meta atingida:</span>
+          <span>192%</span>
+        </div>
+        <div class="item-table" style="border: none; color: #24A527">
+          <i class="fi fi-sr-sack-dollar"></i>
+          <span>Comissão:</span>
+          <span>R$202,20</span>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ProjectionSection",
+  props: {
+    mode: {
+      required: true
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+
+.projection {
+  padding: 0 1vw;
+  @include flex(column, flex-start, initial, 10px);
+  .data-projection {
+    width: 35%;
+    background-color: #fff;
+    border-radius: 5px;
+    @include sh;
+    @include flex(column, flex-start, initial, 10px);
+    padding: 2vh 1vw;
+    border: 1px solid #fff;
+
+    p {
+      font-size: 1.6rem;
+      font-weight: 400;
+      color: $ml-text-menu;
+    }
+
+    .table-projection {
+      @include flex(column, flex-start, initial, 10px);
+
+      .item-table {
+        width: 100%;
+        @include flex(row, flex-start, center, 5px);
+        padding: 2vh 0;
+        border-bottom: 2px solid #cccccc50;
+        color: #FEA11D;
+        font-size: 1.6rem;
+        font-weight: 600;
+
+        i {
+          font-size: 2rem;
+        }
+      }
+    }
+  }
+}
+
+.mode-d {
+  h1 {
+    color: $md-text-h1 !important;
+  }
+
+  .data-projection {
+    background-color: $md-back-l;
+    border-color: $age-or !important;
+
+    p {
+      color: $md-text-light;
+    }
+
+    .table-projection {
+
+
+      .item-table {
+
+        border-color: rgba(70, 70, 70, 0.17);
+      }
+    }
+  }
+}
+
+</style>
--- src/views/intranet/ageRv/app/commercial/dashboard/SalesPage.vue
+++ src/views/intranet/ageRv/app/commercial/dashboard/SalesPage.vue
@@ -14,111 +14,118 @@
       <div id="content-page"
            :class="{'mode-l-p' : mode === 'light'  || mode === undefined,
                   'mode-d-p' : mode === 'dark'}">
-        <div class="dashboard-commercial">
-          <h1>Dashboard de vendas</h1>
-          <div class="dashboards">
-            <div class="dashboard"
-                style="background-color: #FEA11D; cursor: pointer;"
-                @click="modalView('stars')">
-              <div class="type-value">
-                <span>{{ data.stars }}</span>
-                <span>Estrelas</span>
+        <div class="section-1">
+          <div class="dashboard-commercial">
+            <h1>Dashboard de vendas</h1>
+            <div class="dashboards">
+              <div class="dashboard"
+                   style="background-color: #FEA11D; cursor: pointer;"
+                   @click="modalView('stars')">
+                <div class="type-value">
+                  <span>{{ data.stars }}</span>
+                  <span>Estrelas</span>
+                </div>
+                <i class="fi fi-ss-star"></i>
               </div>
-              <i class="fi fi-ss-star"></i>
-            </div>
-            <div class="dashboard"
-                 style="background-color: #2fa1d0; cursor: pointer;"
-                 @click="modalView('salesTotals')">
-              <div class="type-value">
-                <span>{{ data.salesTotals }}</span>
-                <span>Vendas totais</span>
+              <div class="dashboard"
+                   style="background-color: #2fa1d0; cursor: pointer;"
+                   @click="modalView('salesTotals')">
+                <div class="type-value">
+                  <span>{{ data.salesTotals }}</span>
+                  <span>Vendas totais</span>
+                </div>
+                <i class="fi fi-ss-rocket-lunch"></i>
               </div>
-              <i class="fi fi-ss-rocket-lunch"></i>
-            </div>
-            <div class="dashboard" style="background-color: #932b91">
-              <div class="type-value">
-                <span>{{ data.meta }}</span>
-                <span>Meta</span>
+              <div class="dashboard" style="background-color: #932b91">
+                <div class="type-value">
+                  <span>{{ data.meta }}</span>
+                  <span>Meta</span>
+                </div>
+                <i class="fi fi-rr-chart-histogram"></i>
               </div>
-              <i class="fi fi-rr-chart-histogram"></i>
-            </div>
-            <div class="dashboard" style="background-color: #F44336">
-              <div class="type-value">
-                <span>{{ data.cancelTotals }}</span>
-                <span>Cancelamentos<br>Totais</span>
+              <div class="dashboard" style="background-color: #F44336">
+                <div class="type-value">
+                  <span>{{ data.cancelTotals }}</span>
+                  <span>Cancelamentos<br>Totais</span>
+                </div>
+                <i class="fi fi-ss-delete-document"></i>
               </div>
-              <i class="fi fi-ss-delete-document"></i>
-            </div>
-            <div class="dashboard" style="background-color: #FECA1D">
-              <div class="type-value">
-                <span>R${{ data.valueStars }}</span>
-                <span>Valor da estrela</span>
+              <div class="dashboard" style="background-color: #FECA1D">
+                <div class="type-value">
+                  <span>R${{ data.valueStars }}</span>
+                  <span>Valor da estrela</span>
+                </div>
+                <i class="fi fi-sr-grin-stars"></i>
               </div>
-              <i class="fi fi-sr-grin-stars"></i>
-            </div>
-            <div class="dashboard"
-                 style="background-color: #6892d0; cursor: pointer;"
-                 @click="modalView('salesAprovation')">
-              <div class="type-value">
-                <span>{{ data.salesAprovation }}</span>
-                <span>Vendas em <br>Aprovação</span>
+              <div class="dashboard"
+                   style="background-color: #6892d0; cursor: pointer;"
+                   @click="modalView('salesAprovation')">
+                <div class="type-value">
+                  <span>{{ data.salesAprovation }}</span>
+                  <span>Vendas em <br>Aprovação</span>
+                </div>
+                <i class="fi fi-rr-time-twenty-four"></i>
               </div>
-              <i class="fi fi-rr-time-twenty-four"></i>
-            </div>
-            <div class="dashboard" style="background-color: #983FB5">
-              <div class="type-value">
-                <span>{{ data.minMeta }}%</span>
-                <span>Meta mínima </span>
+              <div class="dashboard" style="background-color: #983FB5">
+                <div class="type-value">
+                  <span>{{ data.minMeta }}%</span>
+                  <span>Meta mínima </span>
+                </div>
+                <i class="fi fi-sr-chart-line-up"></i>
               </div>
-              <i class="fi fi-sr-chart-line-up"></i>
-            </div>
-            <div class="dashboard"
-                 style="background-color: #911515; cursor: pointer;"
-                 @click="modalView('cancelD7')">
-              <div class="type-value">
-                <span>{{ data.cancelD7 }}</span>
-                <span>Cancelamento <br> -7 Dias</span>
+              <div class="dashboard"
+                   style="background-color: #911515; cursor: pointer;"
+                   @click="modalView('cancelD7')">
+                <div class="type-value">
+                  <span>{{ data.cancelD7 }}</span>
+                  <span>Cancelamento <br> -7 Dias</span>
+                </div>
+                <i class="fi fi-sr-ban"></i>
               </div>
-              <i class="fi fi-sr-ban"></i>
-            </div>
-            <div class="dashboard" style="background-color: #24A527">
-              <div class="type-value">
-                <span>R${{ data.commission }}</span>
-                <span>Comissão</span>
+              <div class="dashboard" style="background-color: #24A527">
+                <div class="type-value">
+                  <span>R${{ data.commission }}</span>
+                  <span>Comissão</span>
+                </div>
+                <i class="fi fi-sr-sack-dollar"></i>
               </div>
-              <i class="fi fi-sr-sack-dollar"></i>
-            </div>
-            <div class="dashboard"
-                 style="background-color: #009688; cursor: pointer;"
-                 @click="modalView('salesValid')">
-              <div class="type-value">
-                <span>{{ data.sales }}</span>
-                <span>Vendas <br> comissionáveis</span>
+              <div class="dashboard"
+                   style="background-color: #009688; cursor: pointer;"
+                   @click="modalView('salesValid')">
+                <div class="type-value">
+                  <span>{{ data.sales }}</span>
+                  <span>Vendas <br> comissionáveis</span>
+                </div>
+                <i class="fi fi-rr-trophy"></i>
               </div>
-              <i class="fi fi-rr-trophy"></i>
-            </div>
-            <div class="dashboard" style="background-color: #c571e1">
-              <div class="type-value">
-                <span>{{ data.metaPercent }}%</span>
-                <span>Meta atingida</span>
+              <div class="dashboard" style="background-color: #c571e1">
+                <div class="type-value">
+                  <span>{{ data.metaPercent }}%</span>
+                  <span>Meta atingida</span>
+                </div>
+                <i class="fi fi-sr-chart-line-up"></i>
               </div>
-              <i class="fi fi-sr-chart-line-up"></i>
-            </div>
-            <div class="dashboard" style="background-color: #B3B4B5">
-              <div class="type-value">
-                <span>{{ data.deflator }}%</span>
-                <span>Deflator</span>
+              <div class="dashboard" style="background-color: #B3B4B5">
+                <div class="type-value">
+                  <span>{{ data.deflator }}%</span>
+                  <span>Deflator</span>
+                </div>
+                <i class="fi fi-sr-settings-sliders"></i>
               </div>
-              <i class="fi fi-sr-settings-sliders"></i>
             </div>
           </div>
+          <div class="filters">
+              <span>Selecione o mês</span>
+              <div class="buttons-filter">
+                <span :class="{ 'active' : filter.month === '07'}" @click="getSellers('07')">Julho</span>
+                <span :class="{ 'active' : filter.month === '08'}" @click="getSellers('08')">Agosto</span>
+              </div>
+            </div>
         </div>
-        <div class="filters">
-          <span>Selecione o mês</span>
-          <div class="buttons-filter">
-            <span :class="{ 'active' : filter.month === '07'}" @click="getSellers('07')">Julho</span>
-            <span :class="{ 'active' : filter.month === '08'}" @click="getSellers('08')">Agosto</span>
-          </div>
+        <div class="section-2">
+          <ProjectionSection
+            :mode="mode"
+          />
         </div>
       </div>
     </div>
@@ -297,12 +304,13 @@ import MenuApp from "@/components/portal/_aux/MenuApp";
 import HeaderApp from "@/components/portal/_aux/HeaderApp";
 import Cookie from "js-cookie";
 import {AXIOS} from "../../../../../../../services/api.ts";
-
+import ProjectionSection from "@/components/ageRv/dashboards/ProjectionSection";
 export default {
   name: "SalesPage",
   components: {
     MenuApp,
-    HeaderApp
+    HeaderApp,
+    ProjectionSection
   },
   data () {
     return {
@@ -360,101 +368,111 @@ export default {
 }
 
 #content-page {
-  @include flex(row, space-between, initial, 0px);
+  @include flex(column, flex-start, initial, 20px);
+  padding: 2vh 3vw 4vh 3vw !important;
 
-  .dashboard-commercial {
-    @include container(85%, initial, 1vh 1vw, transparent);
+  .section-1 {
+    @include flex(row, space-between, initial, 0px);
 
-    .dashboards {
-      @include container(100%, initial, 1vh 2px, transparent);
-      @include flex(row, flex-start, initial, 20px);
-      flex-wrap: wrap;
+    .dashboard-commercial {
+      @include container(85%, initial, 1vh 1vw, transparent);
 
-      .dashboard {
-        width: calc((100% / 4) - 20px);
-        height: 13vh;
-        background-color: #fff;
-        border-radius: 5px;
-        @include sh-h;
-        @include tr;
-        padding: 2vh 1vw;
-        @include flex(row, space-between, center, 0px);
+      .dashboards {
+        @include container(100%, initial, 1vh 2px, transparent);
+        @include flex(row, flex-start, initial, 20px);
+        flex-wrap: wrap;
 
-        .type-value {
-          @include flex(column, flex-start, initial, 5px);
-          color: #fff;
-          font-weight: 600;
-          span:nth-child(1) {
-            font-size: 2rem;
+        .dashboard {
+          width: calc((100% / 4) - 20px);
+          height: 13vh;
+          background-color: #fff;
+          border-radius: 5px;
+          @include sh-h;
+          @include tr;
+          padding: 2vh 1vw;
+          @include flex(row, space-between, center, 0px);
+
+          .type-value {
+            @include flex(column, flex-start, initial, 5px);
+            color: #fff;
+            font-weight: 600;
+
+            span:nth-child(1) {
+              font-size: 2rem;
+            }
+
+            span:nth-child(2) {
+              font-size: 1.2rem;
+              font-weight: 500;
+              letter-spacing: .6px;
+            }
           }
 
-          span:nth-child(2) {
-            font-size: 1.2rem;
-            font-weight: 500;
-            letter-spacing: .6px;
+          i {
+            font-size: 4rem;
+            color: hsla(0, 0%, 100%, .502);
           }
         }
 
-        i {
-          font-size: 4rem;
-          color: hsla(0,0%,100%,.502);
-        }
       }
 
     }
 
-  }
+    .filters {
+      width: 15%;
+      height: 25%;
+      background-color: #fff;
+      margin-top: 6vh;
+      border-radius: 5px;
+      @include sh;
+      @include flex(column, flex-start, center, 10px);
+      padding: 2vh 2vw;
+      border: 1px solid #fff;
 
-  .filters {
-    width: 15%;
-    height: 17%;
-    background-color: #fff;
-    margin-top: 6vh;
-    border-radius: 5px;
-    @include sh;
-    @include flex(column, flex-start, center, 10px);
-    padding: 2vh 2vw;
-    border: 1px solid #fff;
-
-    span {
-      font-size: 1.2rem;
-      color: $ml-text-light;
-      font-weight: 500;
-    }
-
-    .buttons-filter {
-      @include flex(row, flex-start, center, 10px);
       span {
         font-size: 1.2rem;
-        padding: 5px 10px;
-        border-radius: 5px;
-        color: $age-bl;
-        border: 2px solid $age-bl;
-        @include tr-p;
+        color: $ml-text-light;
+        font-weight: 500;
       }
-    }
-    button {
-      border: 1px solid $age-bl;
-      background-color: $age-bl;
-      color: #fff;
-      padding: 7px .5vw;
-      border-radius: 3px;
-      font-weight: 500;
-      @include tr-p;
-      margin-top: 2vh;
 
-      &:hover {
-        background-color: #fff;
-        color: $age-bl;
+      .buttons-filter {
+        @include flex(row, flex-start, center, 10px);
+
+        span {
+          font-size: 1.2rem;
+          padding: 5px 10px;
+          border-radius: 5px;
+          color: $age-bl;
+          border: 2px solid $age-bl;
+          @include tr-p;
+        }
+      }
+
+      button {
         border: 1px solid $age-bl;
-      }
-    }
+        background-color: $age-bl;
+        color: #fff;
+        padding: 7px .5vw;
+        border-radius: 3px;
+        font-weight: 500;
+        @include tr-p;
+        margin-top: 2vh;
 
-    .active {
+        &:hover {
+          background-color: #fff;
+          color: $age-bl;
+          border: 1px solid $age-bl;
+        }
+      }
+
+      .active {
         background-color: $age-bl;
         color: #fff !important;
+      }
     }
+
   }
+
 }
 
 .stars {
