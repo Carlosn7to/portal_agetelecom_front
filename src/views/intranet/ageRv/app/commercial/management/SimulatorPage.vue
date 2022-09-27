f4e9e0b4e51959ef54663b97da898cac83b9422b
Carlos Neto <carlosn7to@gmail.com>
2022-09-27 07:42:17 -0300

    mudanças

--- src/views/intranet/ageRv/app/commercial/management/SimulatorPage.vue
+++ src/views/intranet/ageRv/app/commercial/management/SimulatorPage.vue
@@ -11,31 +11,14 @@
       <div id="content-page"
            :class="{'mode-l-p' : mode === 'light'  || mode === undefined,
                   'mode-d-p' : mode === 'dark'}">
+
         <template v-if="stage === 'supervisors'">
           <div class="items-header">
-            <div class="item" style="justify-content: flex-start">
-              <span>Supervisor</span>
+            <div class="item" style="justify-content: flex-start; width: 50%">
+              <span>Canal</span>
             </div>
             <div class="item">
               <span>Vendas</span>
-            </div>
-            <div class="item">
-              <span>Meta</span>
-            </div>
-            <div class="item">
-              <span>Meta atingida</span>
-            </div>
-            <div class="item">
-              <span>Canceladas</span>
-            </div>
-            <div class="item">
-              <span>Estrelas</span>
-            </div>
-            <div class="item">
-              <span>Valor da estrela</span>
-            </div>
-            <div class="item">
-              <span>Acelerador/ <br> Deflator</span>
             </div>
             <div class="item">
               <span>Comissão</span>
@@ -46,32 +29,14 @@
           </div>
           <div class="container-body">
             <div class="items-body">
-              <div class="item" style="justify-content: flex-start">
-                <span>Carlos</span>
+              <div class="item" style="justify-content: flex-start; width: 50%">
+                <span>MCV</span>
               </div>
               <div class="item">
-                <span>201</span>
+                <span>1042</span>
               </div>
               <div class="item">
-                <span>548</span>
-              </div>
-              <div class="item">
-                <span>100%</span>
-              </div>
-              <div class="item">
-                <span>54</span>
-              </div>
-              <div class="item">
-                <span>2000</span>
-              </div>
-              <div class="item">
-                <span>R$4.5</span>
-              </div>
-              <div class="item">
-                <span>10%</span>
-              </div>
-              <div class="item">
-                <span>R$5242.20</span>
+                <span>R$35.293,11</span>
               </div>
               <div class="item" style="gap: 5px">
                 <i class="fi fi-rr-info" @click="extractView('supervisor', item)"></i>
@@ -83,6 +48,35 @@
       </div>
     </div>
   </div>
+  <div id="modal">
+    <div id="card-modal">
+      <button @click="newField()">Add nova faixa</button>
+      <br>
+      <br>
+      <form action="" @submit.prevent="getSimulator" >
+        <template v-for="(field, key) in rulesRange || []" :key="key">
+          <div class="double-items">
+            <span v-if="rulesRange.length !== 1 && key === 0">Faixa inicial</span>
+            <span v-if="key > 0 && (rulesRange.length - 1) !== key">Faixa intermediária</span>
+            <span v-if="(rulesRange.length - 1) === key"> Faixa final</span>
+            <div class="double-item">
+              <label for="">Inicial {{ key }}</label>
+              <input type="number" name="" id="" v-model="rulesRange[key].first">
+            </div>
+            <div class="double-item" v-if="(rulesRange.length - 1) !== key">
+              <label for="">Final</label>
+              <input type="number" name="" id="" v-model="rulesRange[key].last">
+            </div>
+            <div class="double-item">
+              <label for="">Valor</label>
+              <input type="number" name="" id="" v-model="rulesRange[key].value">
+            </div>
+          </div>
+        </template>
+        <input type="submit" value="Enviar" v-if="rulesCount > 0">
+      </form>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -90,6 +84,7 @@
 import MenuApp from "@/components/portal/_aux/MenuApp";
 import HeaderApp from "@/components/portal/_aux/HeaderApp";
 import Cookie from "js-cookie";
+import {AXIOS} from "../../../../../../../services/api.ts";
 
 export default {
   name: "SimulatorPage",
@@ -100,12 +95,41 @@ export default {
   data () {
     return {
       mode: Cookie.get('mode'),
-      stage: 'supervisors'
+      stage: 'supervisors',
+      rulesRange: [],
+      rulesCount: 0
     }
   },
   methods: {
     modeView: function (mode) {
       this.mode = mode
+    },
+    getSimulator: function () {
+
+      AXIOS({
+        method: 'GET',
+        url: '/teste',
+        params: {
+          rulesRange: {
+            mcv: this.mcv
+          }
+        }
+      }).then((res) => {
+        console.log(res.data)
+      }).catch((error) => {
+        console.log(error)
+      })
+    },
+    newField: function () {
+
+        this.rulesRange.push({
+          count: `1º - Faixa`,
+          first: 0,
+          last: 0,
+          value: 0
+        })
+      this.rulesCount++
+
     }
   },
   mounted() {
@@ -131,6 +155,34 @@ export default {
 .mode-d-p {
   background-color: #161819;
   @include tr;
+}
+
+#modal {
+  #card-modal {
+    width: 50vw;
+    height: 90vh;
+    padding: 3vh 1vw;
+    max-height: 90vh;
+    overflow-y: auto;
+
+    form {
+      @include flex(column, flex-start, initial, 10px);
+      width: 60%;
+
+      .double-items {
+        @include flex(column, flex-start, initial, 10px);
+        span {
+          font-size: 1.2rem;
+          color: $age-bl;
+        }
+
+        .double-item {
+          @include flex(column, flex-start, initial, 10px);
+        }
+
+      }
+    }
+  }
 }
 
 </style>
