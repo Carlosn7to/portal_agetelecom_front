9f76e96192cb885c22fd440ea3cc546aa4ce7dc1
Carlos Alberto M. Da F. Neto <carlos.neto@tote.local>
2023-07-13 12:37:22 -0300

    capacity

--- src/components/ageTools/tools/schedule/capacity/CapacitySchedule.vue
+++ src/components/ageTools/tools/schedule/capacity/CapacitySchedule.vue
@@ -14,8 +14,26 @@ export default defineComponent({
       filters: {
 
       },
-      dataItems: null
+      dataItems: null,
+      modal: false,
+      dataModal: {
+        typeNote: '',
+        turn: '',
+        clients: []
+      },
+      search: ''
     }
+  },
+  computed: {
+    ClientsFiltered: function () {
+      let values = []
+      values = this.dataModal.clients[0].filter((value) => {
+        return (
+            value.toLowerCase().indexOf(this.search.toLowerCase()) > -1
+        )
+      })
+      return values
+    },
   },
   methods: {
     getDataCapacity(day) {
@@ -33,6 +51,12 @@ export default defineComponent({
     },
     returnPage () {
       this.$emit('returnPage')
+    },
+    openModalClients (typeNote, turn, clients) {
+      this.modal = true;
+      this.dataModal.typeNote = typeNote;
+      this.dataModal.turn = turn;
+      this.dataModal.clients = clients;
     }
   }
 })
@@ -90,18 +114,100 @@ export default defineComponent({
           </div>
           <div class="turns">
             <div class="turn">
-              <span>Manhã: <b>{{ item.manha }}</b></span>
+              <span>Manhã: <b>{{ item.manha.count }}</b></span>
+              <i class="fi fi-rr-rectangle-list" @click="openModalClients(index, 'Manhã', item.manha.clients)"></i>
             </div>
             <div class="turn">
-              <span>Tarde: <b>{{ item.tarde }}</b></span>
+              <span>Tarde: <b>{{ item.tarde.count }}</b></span>
+              <i class="fi fi-rr-rectangle-list" @click="openModalClients(index, 'Tarde', item.tarde.clients)"></i>
             </div>
           </div>
       </div>
     </div>
   </div>
+  <div class="modal" v-if="modal">
+    <div class="card-modal">
+      <div class="title-exit">
+        <span>Listagem de clientes - {{ dataModal.typeNote }} - Período {{ dataModal.turn }}</span>
+        <i class="fi fi-rr-cross" @click="modal = false"></i>
+      </div>
+
+      <div class="search">
+        <input type="text" name="search" id="search" placeholder="Buscar cliente..." autocomplete="off" v-model="search">
+      </div>
+
+      <div class="list">
+        <div class="client" v-for="(client, index) in ClientsFiltered" :key="index">
+          <span>{{ client }}</span>
+        </div>
+      </div>
+
+    </div>
+  </div>
 </template>
 
 <style scoped lang="scss">
+
+.modal {
+  width: 50vw;
+
+  position: absolute;
+  top: 18%;
+  left: 2vw;
+  z-index: 99;
+
+  .card-modal {
+    @include card();
+    min-height: 70vh;
+    max-height: 70vh;
+
+    @include flex(column, flex-star, initial, 3vh);
+
+    .search {
+      input {
+        @include card();
+        padding: 10px 5px;
+        width: 40%;
+        font-size: 1rem;
+      }
+
+      ::placeholder {
+        font-size: 1rem;
+      }
+    }
+
+
+    .title-exit {
+      @include flex(row, space-between, center, 0);
+      span {
+        font-size: 1.4rem;
+        font-weight: 600;
+      }
+      i {
+        font-size: 1.2rem;
+        color: #333;
+        cursor: pointer;
+
+        &:hover {
+          opacity: .8;
+        }
+      }
+    }
+
+
+    .list {
+      @include flex(column, flex-start, initial, .5vh);
+      max-height: 70vh;
+      overflow-y: auto;
+      padding: 2vh 0;
+
+      .client {
+        padding: 1vh 0;
+        border-bottom: 1px solid #33333330;
+      }
+    }
+  }
+}
 
 .modal-filters {
   @include flex(column, flex-start, initial, 2vh);
@@ -170,6 +276,19 @@ export default defineComponent({
         @include flex(column,flex-start,initial, 1vh);
 
         .turn {
+          @include flex(row, flex-start, center, 1vw);
+
+          i {
+            font-size: 1.2rem;
+            color: $primary;
+            margin-left: auto;
+            cursor: pointer;
+
+            &:hover {
+              opacity: .8;
+            }
+          }
+
           b {
             font-size: 1.2rem;
           }
